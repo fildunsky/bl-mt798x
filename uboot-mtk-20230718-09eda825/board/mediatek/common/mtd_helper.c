5eb891bb1391ac07022cdd01c69d9b79cb67b5c6
Fil Dunsky <32987666+fildunsky@users.noreply.github.com>
2026-02-16 17:48:16 +0300

    uboot: restore original rootfs_data creation in default ubi2 tar path

--- uboot-mtk-20230718-09eda825/board/mediatek/common/mtd_helper.c
+++ uboot-mtk-20230718-09eda825/board/mediatek/common/mtd_helper.c
@@ -886,7 +886,7 @@ static int write_ubi2_tar_image(const void *data, size_t size,
 
 	return ret;
 #else
-	return create_rootfs_data_volume();
+	return create_ubi_volume(PART_ROOTFS_DATA_NAME, 0, -1, true);
 #endif /* CONFIG_MTK_DUAL_BOOT */
 }
 
